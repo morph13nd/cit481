fefab695acf98f73bbcf9552c506632396a311d4
David Galstyan <68514316+davidgalstyan53@users.noreply.github.com>
2021-04-18 09:46:18 -0700

    Update nginx_docker_install.sh

--- nginx_docker_install.sh
+++ nginx_docker_install.sh
@@ -2,13 +2,14 @@
 
 #startup script to install nginx, docker, and docker-compose
 
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get install nginx
 sudo systemctl restart nginx; wait 5; curl localhost
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y update
 
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
