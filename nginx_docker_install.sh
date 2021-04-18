cffb7f0e75c9e097b5d3ed9cc770f15bfd445195
David Galstyan <68514316+davidgalstyan53@users.noreply.github.com>
2021-04-18 09:21:15 -0700

    Run script to install Nginx and Docker

---
+++ nginx_docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#startup script to install nginx, docker, and docker-compose
+
+sudo apt-get update
+sudo systemctl restart nginx; wait 5; curl localhost
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ${USER}
+
+ sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ sudo chmod +x /usr/local/bin/docker-compose
+
+echo 'Run "docker --version" to check installation for Docker'
+echo 'Exit SSH session and re-enter for changes to take effect"
