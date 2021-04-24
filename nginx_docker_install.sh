a524c06695cebcfde97d38a3a949ef92d2284c55
David Galstyan <68514316+davidgalstyan53@users.noreply.github.com>
2021-04-23 21:02:39 -0700

    Update nginx_docker_install.sh

--- nginx_docker_install.sh
+++ nginx_docker_install.sh
@@ -14,8 +14,8 @@ sudo apt-get -y update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
 
- sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
- sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo 'Run "docker --version" to check installation for Docker'
 echo 'Exit SSH session and re-enter for changes to take effect'
