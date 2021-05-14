f8ad0929ba6e26cb4ea939426de212b3da832532
themaverick <35180652+themaverick@users.noreply.github.com>
2021-05-14 14:09:56 -0700

    Update pro_launch.sh

--- Prometheus/pro_launch.sh
+++ Prometheus/pro_launch.sh
@@ -1,1 +1,39 @@
+#!/bin/bash
 
+
+sudo apt-get -y update
+
+git clone https://github.com/themaverick/cit481.git
+cd cit481
+
+
+
+sudo useradd --no-create-home prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.19.0/prometheus-2.19.0.linux-amd64.tar.gz
+tar xvfz prometheus-2.19.0.linux-amd64.tar.gz
+sudo cp prometheus-2.19.0.linux-amd64/prometheus /usr/local/bin
+sudo cp prometheus-2.19.0.linux-amd64/promtool /usr/local/bin/
+sudo cp -r prometheus-2.19.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-2.19.0.linux-amd64/console_libraries /etc/prometheus
+sudo cp prometheus-2.19.0.linux-amd64/promtool /usr/local/bin/
+rm -rf prometheus-2.19.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+vim /etc/prometheus/prometheus.yml
+caftg /etc/prometheus/prometheus.yml
+cat /etc/prometheus/prometheus.yml
+sudo vim /etc/prometheus/prometheus.yml
+cat /etc/prometheus/prometheus.yml
+sudo vim /etc/systemd/system/prometheus.service
+cat /etc/systemd/system/prometheus.service
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+sudo promtool check /etc/prometheus/prometheus.yml
+sudo promtool check config /etc/prometheus/prometheus.yml
