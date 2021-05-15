5dd1d049e421080bdc982f00805fabc5a809cf8a
lostBoysLeader <68514305+lostBoysLeader@users.noreply.github.com>
2021-05-15 11:54:58 -0700

    Update grafanalaunch.sh
    revert to recreating grafana

--- TF/grafanalaunch.sh
+++ TF/grafanalaunch.sh
@@ -9,12 +9,8 @@ sudo apt-get -y update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
 
-#pull existing grafana container
-
-sudo docker login -u cit481 -p 123456789
-sudo docker pull cit481/grafana:latest
-sudo docker run -itd -p 3000:3000 cit481/grafana:latest
-
+#install grafana container
+sudo docker run -d --name=grafana -p 3000:3000 grafana/grafana
 echo "PLEASE WAIT, INITIALIZING CONTAINERS..."
 
 sleep 10
