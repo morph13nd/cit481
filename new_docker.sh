5ef0f48d99e12cb2339c61b06b20ab2d64af848c
lostBoysLeader <68514305+lostBoysLeader@users.noreply.github.com>
2021-05-15 14:43:11 -0700

    Update new_docker.sh

--- new_docker.sh
+++ new_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo docker login -u cit481 -p 123456789
-sudo docker pull cit481/mongodb:latest
 sudo docker stop rocketchat_mongo_1
 sudo docker system prune -a
 sudo docker run --expose 27017 -itd cit481/mongodb:latest /bin/bash
